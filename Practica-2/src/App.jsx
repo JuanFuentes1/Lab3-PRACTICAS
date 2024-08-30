e35b47250fa774a109e56d95da1a3a9b003d91ac
JuanFuentes1 <juanfuentes3168a@gmail.com>
2024-08-30 18:09:26 -0300

    Practica 2

---
+++ Practica-2/src/App.jsx
@@ -0,0 +1,42 @@
+import React from 'react'
+import Tablee from "./Tablee"
+import Table from "react-bootstrap/Table"
+
+
+function App() {
+
+  const netIncomes = [
+    { brand: "McDonalds", income: 1291283 },
+    { brand: "Burger King", income: 1927361 },
+    { brand: "KFC", income: 1098463 },
+  ];
+
+  const incomes = netIncomes.map((inc) => (inc.income))
+  const accumIncomes = incomes.reduce((accumulator, currentValue) => accumulator + currentValue, 0)
+  const averageIncomes = accumIncomes/incomes.length
+
+  return (
+    <div className='container'>
+      <Table striped bordered hover>
+        <thead>
+          <tr>
+            <th>Nombre</th>
+            <th>Ing. brutos</th>
+          </tr>
+        </thead>
+        <tbody>
+          {netIncomes.map((item, index)=>(
+            <Tablee 
+            key={index}
+            brand={item.brand}
+            income={item.income}
+            />
+        ))}
+        </tbody>
+      </Table>
+      <p>El promedio de ingresos brutos es de {averageIncomes.toFixed(2)}</p>
+    </div>
+  )
+}
+
+export default App
